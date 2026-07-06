c5d553e485e678709ef3b9acf0188f9e1c3fecde
vashurathour <vashu2002rathour@gmail.com>
2026-07-06 13:35:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app    = express()
 const server = http.createServer(app)
 const io     = new Server(server, { cors: { origin: '*' } })
 
-app.use(cors())
+app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('io', io)
